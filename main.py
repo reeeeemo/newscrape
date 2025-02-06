74bf573fb297901d1daa0baa4bb5727a78602894
reeeeemo <robert@oxley.ca>
2025-02-06 01:31:22 -0500

    generalized webfeed + added cbc

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import pandas as pd
-import feedparser
 from dataclasses import dataclass
-from bs4 import BeautifulSoup
-import requests
-from webfeeds import GovernmentWebFeed
+from webfeeds import CBCWebFeed, GovernmentWebFeed
 import os
+
+DEBUG = True
 
 # makes output if it doesnt exist as a folder
 os.makedirs("output", exist_ok=True)
-
 pd.set_option("display.max_columns", None)  # Ensure all columns are visible
 pd.set_option("display.max_colwidth", None) # Preventing truncuation
 pd.set_option("display.width", 200) # Increase console width
 
-webfeed = GovernmentWebFeed()
+# Making all webfeeds into a varialbe
+govfeed = GovernmentWebFeed()
+cbcfeed = CBCWebFeed()
 
-news_data = webfeed.get_webfeed(["drugs", "drug"], ["fentanyl"])
-
- 
+# Getting all webfeeds according to word(s)
+news_data = []
+# news_data.update(govfeed.get_webfeed(["banana", "banana"], ["banana"]))
+news_data.extend(cbcfeed.get_webfeed(["drugs", "drug"], ["fentanyl"]))
 df = pd.DataFrame(news_data)
 
-print(df)
+if DEBUG:
+    print(df)
 
-
-df.to_csv('output/news_articles.csv')
+# Print to a .csv file
+#df.to_csv('output/news_articles.csv')
--- webfeeds.py
+++ webfeeds.py
@@ -1,54 +1,143 @@
 import pandas as pd
 import feedparser
 from dataclasses import dataclass, field
+from typing import Literal
 from bs4 import BeautifulSoup
 import requests
 
-
+'''
+    Parent class for all WebFeeds
+'''
 @dataclass
-class GovernmentWebFeed:
-    base_url: str = "https://api.io.canada.ca/io-server/gc/news/en/v2?sort=publishedDate&orderBy=desc&publishedDate%3E=2024-01-25&pick=100&format=atom&atomtitle="
-    provinces: list[str] = field(default_factory=lambda: ["National%20News", "Alberta", "Ontario", "British%20Columbia", "Manitoba", "New%20Brunswick", 
-             "Newfoundland%20and%20Labrador", "Northwest%20Territories", "Nova%20Scotia",
-             "Nunavut", "Prince%20Edward%20Island", "Quebec", "Saskatchewan", "Yukon"])
+class WebFeed:
+    base_url: str = field(default_factory=str)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+    parser_type: Literal["html", "xml"] = "html"
     
-    def get_url(self, title: str = "National%20News") -> str:
-        return f'{self.base_url}{title}'
-    
-    def get_webfeed(self, title_words: list[str], feed_words: list[str]) -> list[dict]:
-        news_data = []
-        seen_links = set()
-        for prov in self.provinces:
-            rss_url = self.get_url(prov) 
-            feed = feedparser.parse(rss_url) # get and parse webfeed
-            # for each news article
-            keyword_entries = self.check_feed_for_word(title_words, feed.entries)
-            
-            for entry in keyword_entries:
-                if entry.id in seen_links:
-                    continue
-                response = requests.get(entry.link)
-                if response.status_code != 200:
-                    print(f'Error getting url {entry.link}: {response.status_code}. Skipping...')
-                    continue
-                soup = BeautifulSoup(response.content, 'html.parser')
-                page_text = soup.body.get_text().lower() if soup.body else ""
-                if any(word in page_text for word in feed_words): # check to ensure there is talk of fentanyl within
-                    news_data.append({
-                        'Province': prov.replace("%20", " "),
-                        'Title': entry.title,
-                        'Link': entry.id
-                    })
-                    seen_links.add(entry.id)
-        return news_data
+    '''
+        From the base_url given, get all of the RSS or ATOM web feeds
+        
+        Returns a set of RSS/ATOM links
+    '''
+    def get_urls(self) -> set[str]:
+        try:
+            response = requests.get(self.base_url,headers=self.headers, timeout=10)
+            response.raise_for_status()
+        except requests.Timeout:
+            print(f'Timeout error: {self.base_url}. Skipping...')
+            return set()
+        except requests.RequestException as e:
+            print(f'Error getting url {self.base_url}: {e}')
+            return set()
 
+        soup = BeautifulSoup(response.content, f'{self.parser_type}.parser')
+        links = [a.get("href") for a in soup.find_all("a") if a.get("href")]
+        links = [link for link in links if 'rss' in link or 'atom' in link]
+        return set(links)
+    
+    '''
+        In every feed, check if the title of the entry has any keywords given
+        
+        Returns a list of entries
+    '''
     def check_feed_for_word(self, words: list[str], entries) -> list:
         keyword_entries = []
         if not words or not entries:
             return []
             
         for entry in entries:
-            if any(word in entry.title.lower() for word in words):
+            if any(word in entry['title'].lower() for word in words):
                 keyword_entries.append(entry)
         return keyword_entries
-               
+    
+    '''
+        In every entry, check if the body of the article has any keywords given
+        
+        Returns boolean
+    '''
+    def check_entry_for_word(self, words: list[str], link) -> bool:
+        try:
+            response = requests.get(link, headers=self.headers, timeout=10)
+            response.raise_for_status()
+        except requests.Timeout:
+            print(f'Timeout error: {link}. Skipping...')
+            return False
+        except requests.RequestException as e:
+            print(f'Error getting entry {link}: {e}')
+            return False
+        
+        # parse the news article itself
+        soup = BeautifulSoup(response.content, 'html.parser')
+        page_text = soup.body.get_text().lower() if soup.body else ""
+        if any(word in page_text for word in words): # check to ensure there is talk of keywords within
+            return True
+        return False
+    
+    def get_webfeed(self, title_words: list[str], feed_words: list[str]) -> list[dict]:
+        urls = self.get_urls()
+        if not urls or not title_words: # if there are no title words to search for, too much data
+            return []
+        
+        news_data = []
+        seen_links = set()
+        for url in urls:
+            try:
+                response = requests.get(url, headers=self.headers, timeout=5)
+                response.raise_for_status()
+            except requests.Timeout:
+                print(f"Timeout on {url}. Skipping...")
+                continue
+            except requests.RequestException as e:
+                print(f"Request failed on {url}: {e}")
+                continue
+            
+            # get feed data (dict of items/entries)
+            feed = feedparser.parse(response.content)
+            
+            entries = getattr(feed, "entries", None) or \
+                getattr(feed.feed, "items", None) or \
+                getattr(feed.feed, "channel", {}).get("item", []) or \
+                feed.get("items", [])
+            
+            if not isinstance(entries, list):
+                print(f"Unexpected format for entries: {type(entries)}. Skipping...")
+                continue
+
+            if not entries:
+                print(f"No entries found for {url}. Skipping...")
+                continue
+            
+            keyword_entries = self.check_feed_for_word(title_words, entries)
+            for entry in keyword_entries:
+                # get the news article
+                link = entry.get("link")
+                if not link:
+                    link = entry.get("id") if entry.get("id") else entry.get("guid")
+                    
+                print(link)
+
+                if link in seen_links:
+                    continue
+                
+                if self.check_entry_for_word(feed_words, link):
+                    news_data.append({
+                        'Title': entry['title'],
+                        'Link': link
+                    })
+                    seen_links.add(link)
+                
+                    
+        return news_data
+    
+
+@dataclass
+class GovernmentWebFeed(WebFeed):
+    base_url: str = "https://www.canada.ca/en/news/web-feeds.html"
+    
+
+
+@dataclass
+class CBCWebFeed(WebFeed):
+    base_url: str = "https://www.cbc.ca/rss/"
